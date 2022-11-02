b0d3ee7571a5c9b800b16def36c67aa3b7ade6d7
huudao2004zz <111514495+huudao2004zz@users.noreply.github.com>
2022-11-02 15:07:56 +0700

    Add files via upload

---
+++ idell/app.js
@@ -0,0 +1,31 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    $scope.users = [
+        {name: "Iphone 14 promax", fullname: "$22.50"},
+        {name: "Iphone 14 pro", fullname: "$15.00"},
+        {name: "Iphone 14 promax", fullname: "$123.82"}
+
+    ];
+    $scope.addUser = function () {
+        $scope.users.push($scope.newUser);
+        $scope.newUser = {};
+        $scope.message = "New User Added successfully";
+
+    };
+   $scope.selectUser = function(user){
+        console.log(user);
+        $scope.clickedUser = user ;
+
+    };
+    $scope.editUser =function (){
+        $scope.message=" Edited successfully";
+};
+    $scope.deleteUser =function (){
+        $scope.users.splice($scope.users.indexOf($scope.clickedUser), 1);
+        $scope.message= " Deleted successfully";
+
+    };
+    $scope.clearMessage =function (){
+        $scope.message="";
+    };
+});
